decb810279f669245abc3fac2efd82f1e9f03fc5
Vladislav <taranov.vlad.1995@gmail.com>
2021-08-31 15:35:21 +0300

    1. test commit

--- src/index.js
+++ src/index.js
@@ -2,43 +2,40 @@ import './css/style.scss'
 const isAnagram = require('./Anagram')
 
 const btn = document.querySelector('.card__btn')
-const inputText = document.querySelector('.card__input')
+const firstInput = document.querySelector('#firstInputValue')
+const secondInput = document.querySelector('#secondInputValue')
 const resultText = document.querySelector('.card__result')
 const errorText = document.querySelector('.error')
 
 btn.addEventListener('click', () => {
-    let inputStr = inputText.value
-    let strArr = inputText.value.split(',')
+    let firstValue = firstInput.value
+    let secondValue = secondInput.value
+
+    event.preventDefault()
+    
     errorText.hidden = false
-    if(inputStr === '') {
-        errorText.textContent = '! Слова не введены'
+    if(firstValue === '' || secondValue === '') {
+        errorText.textContent = 'Ошибка: cлова не введены'
         resultText.textContent = ''
-        inputText.value = ''
+        firstInput.value = ''
+        secondInput.value = ''
     }
-    else if(strArr.length !== 2) {
-        errorText.textContent = '! Требуется ввести 2 слова через запятую'
-        resultText.textContent = ''
-        inputText.value = ''
-    }  
     else {
-        let result = isAnagram(strArr[0], strArr[1])
+        let result = isAnagram(firstValue, secondValue)
 
         errorText.hidden = true
-        inputText.value = ''
-
-        resultText.textContent = result
+        firstInput.value = ''
+        secondInput.value = ''
 
         if(result) {
+            resultText.textContent = `Слова ${firstValue} и ${secondValue} являются анаграмами.`
             resultText.classList.add('card__result-true');
         }
         else {
+            resultText.textContent = `Слова ${firstValue} и ${secondValue} не являются анаграмами.`
             resultText.classList.add('card__result-false');
         }
     }
 })
-
-function anagram(firstStr, secondStr) {
-    return firstStr.replace(/\s+/g, '').split('').sort().join('') === secondStr.replace(/\s+/g, '').split('').sort().join('')
-}
 
 module.exports = anagram;
