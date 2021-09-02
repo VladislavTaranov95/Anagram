471f71414ff8a2f242d51b7ddc76d6ebc159333d
Vladislav <taranov.vlad.1995@gmail.com>
2021-09-02 15:30:05 +0300

    Added some tests

--- src/Anagram.js
+++ src/Anagram.js
@@ -1,5 +1,7 @@
 function isAnagram(firstStr, secondStr) {
-    return firstStr.toLowerCase().replace(/[^a-zа-яё]/gi,"").split('').sort().join('') === secondStr.toLowerCase().replace(/[^a-zа-яё]/gi,"").split('').sort().join('')
+    if(firstStr === secondStr) return false
+    else if(!isNaN(Number(firstStr)) || !isNaN(Number(secondStr))) return false
+    else return firstStr.toLowerCase().replace(/[^a-zа-яё0-9]/gi,"").split('').sort().join('') === secondStr.toLowerCase().replace(/[^a-zа-яё]/gi,"").split('').sort().join('')
 }
 
 module.exports = isAnagram;
